9c619e81e972e8f0f65979e0bbd5b341de025e64
seti <sebastian.koehlmeier@kyberna.com>
2017-01-23 19:38:01 +0100

    config changes

--- rspamd/assets/init.sh
+++ rspamd/assets/init.sh
@@ -5,7 +5,19 @@
 #  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/rspamd/*.conf
 #done
 
-echo "password = \"$RSPAMD_PWD\";" > /etc/rspamd/local.d/worker-controller.inc
+echo "bind_socket = \"*:11334\";" > /etc/rspamd/local.d/worker-controller.inc
+echo "password = \"$RSPAMD_PWD\";" >> /etc/rspamd/local.d/worker-controller.inc
 echo "enable_password = \"$RSPAMD_PWDADMIN\";" >> /etc/rspamd/local.d/worker-controller.inc
+
+if [ ! -e /maps/whitelist.map ];
+then
+    echo "127.0.0.1" >> /maps/whitelist.map
+    echo "192.168.1.1" >> /maps/whitelist.map
+    echo "192.168.2.0/24" >> /maps/whitelist.map
+    echo "172.17.0.0/16" >> /maps/whitelist.map
+fi
+
+chown 106:108 /var/lib/rspamd -R
+chown 106:108 /maps -R
 
 rm -f /var/run/rsyslogd.pid
