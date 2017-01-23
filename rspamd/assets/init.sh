5171cfd5bc0cc813e049a73649635205091ad6af
kse <sebastian.koehlmeier@kyberna.com>
2017-01-23 17:12:01 +0100

    Start Configure Changes
    Moved some configs from rmilter to rspamd cause of disfunction in
    rmilter.

--- rspamd/assets/init.sh
+++ rspamd/assets/init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-cp -f /etc/rspamd/rspamd.tmpl /etc/rspamd/rspamd.conf
+#cp -f /etc/rspamd/rspamd.tmpl /etc/rspamd/rspamd.conf
 # Substitute configuration
-for VARIABLE in `env | cut -f1 -d=`; do
-  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/rspamd/*.conf
-done
+#for VARIABLE in `env | cut -f1 -d=`; do
+#  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/rspamd/*.conf
+#done
+
+echo "password = \"$RSPAMD_PWD\";" > /etc/rspamd/local.d/worker-controller.inc
+echo "enable_password = \"$RSPAMD_PWDADMIN\";" >> /etc/rspamd/local.d/worker-controller.inc
 
 rm -f /var/run/rsyslogd.pid
